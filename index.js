195564f0b1c85fa0d0eef65d2c497822f406ad8a
Sommerfeld <jannik.sommerfeld@icloud.com>
2014-08-01 15:03:11 +0200

    initial

---
+++ index.js
@@ -0,0 +1,35 @@
+/*
+	Short-ID-Generator
+	by Jannik Sommerfeld
+
+	Generate 62base IDs with 6 characters.
+*/
+
+var valueToChar = function(value) {
+	if (value > 9) {
+		var asciiDec = 55 + value
+		if (asciiDec > 90) {
+			asciiDec += 6;
+		}
+		return String.fromCharCode(asciiDec)
+	}
+	else {
+		return value.toString();	
+	}
+}
+
+var valueToString = function(value) {
+	var x = value % 62
+	var y = Math.floor(value/62)
+
+	if (y > 0) {
+		return valueToString(y) + valueToChar(x)
+	} else {
+		return valueToChar(x)
+	}
+}
+
+exports.generate = function () {
+	var random = Math.floor(Math.random()*2000000000)+1000000
+	return valueToString(random)
+}
---
+++ readme.md
@@ -0,0 +1,14 @@
+# shortIDGenerator
+
+Build short IDs (4-6 chars) with numbers, uppercase- and lowercase-letters.
+
+This is a quick'n'dirty-tool. It's only used for public IDs - something like shortURLs - by my side.
+
+## Install
+
+	npm install shortIDGen --save
+
+## Usage
+
+	var shortID = require('shortIDGen')
+	shortID.generate()
